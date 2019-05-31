2ba3186a0477746b668f4edbfd25080c4f4563a1
Mary Herten <hertenmary@gmail.com>
2019-05-31 13:07:08 +1000

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,25 @@ $('.about-close').click(function() {
   $('.about').removeClass('active');
 });
 
+$('.buy-link').click(function() {
+  $('.buy').addClass('active');
+});
+
+$('.buy-close').click(function() {
+  $('.buy').removeClass('active');
+});
+
+$('.articles-link').click(function() {
+  $('.articles').addClass('active');
+});
+
+$('.articles-close').click(function() {
+  $('.articles').removeClass('active');
+});
+
+
+
+
 
 
 
